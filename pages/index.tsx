dec81757139cf1610f52db3d326de603662fd4ae
lmw <alsdndjdrktl@gmail.com>
2022-03-01 19:04:38 +0900

    feat : swr 적용 및 index ssr 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
 import Layout from '@components/layout';
 import type {NextPage} from 'next';
 import Head from 'next/head';
+import client from '@libs/server/client';
+import {Product} from '@prisma/client';
+import {SWRConfig} from 'swr';
+
+export interface ProductWithCount extends Product {
+ _count: {
+  favs: number;
+ };
+}
+
+interface ProductsResponse {
+ ok: boolean;
+ products: ProductWithCount[];
+}
 
 const Home: NextPage = () => {
  return (
@@ -15,4 +29,30 @@ const Home: NextPage = () => {
  );
 };
 
-export default Home;
+const Page: NextPage<{products: ProductsResponse[]}> = ({products}) => (
+ <SWRConfig
+  value={{
+   fallback: {
+    '/api/products': {
+     ok: true,
+     products,
+    },
+   },
+  }}
+ >
+  <Home />
+ </SWRConfig>
+);
+
+export async function getServerSideProps() {
+ console.log('SSR');
+ const products = await client.product.findMany({});
+
+ return {
+  props: {
+   products: JSON.parse(JSON.stringify(products)),
+  },
+ };
+}
+
+export default Page;
