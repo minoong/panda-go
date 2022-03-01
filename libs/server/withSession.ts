aa046713bce379b730f6b10d9a807f4458ce3c96
lmw <alsdndjdrktl@gmail.com>
2022-03-01 16:00:24 +0900

    feat : api and common fn 구현

---
+++ libs/server/withSession.ts
@@ -0,0 +1,23 @@
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
+
+declare module "iron-session" {
+    interface IronSessionData {
+      user?: {
+        id: number;
+      };
+    }
+  }
+
+const cookieOptions = {
+    cookieName: "pandagosession",
+    password: process.env.COOKIE_PASSWORD!,
+  };
+  
+  export function withApiSession(fn: any) {
+    return withIronSessionApiRoute(fn, cookieOptions);
+  }
+  
+  export function withSsrSession(handler: any) {
+    return withIronSessionSsr(handler, cookieOptions);
+  }
+  
---
+++ pages/api/users/enter.ts
@@ -0,0 +1,35 @@
+import { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+
+async function handler(
+    req : NextApiRequest,
+    res : NextApiResponse<ResponseType>
+) {
+    const { phone, email } = req.body
+    const user = phone ? {phone} : email ? {email} : null
+
+    if (!user) return res.status(400).json({ok:false})
+
+    const payload = `${Math.floor(100000 + Math.random() * 900000)}`
+    const token = await client.token.create({
+        data : {
+            payload,
+            user : {
+                connectOrCreate: {
+                    where : {
+                        ...user
+                    },
+                    create : {
+                        name : 'Anonymous',
+                        ...user,
+                    }
+                }
+            }
+        }
+    });
+
+    return res.json({
+        ok:true
+    })
+}
