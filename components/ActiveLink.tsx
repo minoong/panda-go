57a4c74ba62cdfb7a9cfd6e471b8123d27d672e1
lmw <alsdndjdrktl@gmail.com>
2022-03-01 19:55:11 +0900

    refactor : ActiveLink component

--- components/ActiveLink.tsx
+++ components/ActiveLink.tsx
@@ -4,14 +4,15 @@ import {Children, cloneElement, useEffect, useState} from 'react';
 
 interface ActiveLinkProps extends React.PropsWithChildren<LinkProps> {
  activeClassName: string;
+ defaultClassName: string;
  children: React.ReactNode;
 }
 
-export default function ActiveLink({activeClassName, children, ...props}: ActiveLinkProps) {
+export default function ActiveLink({activeClassName, defaultClassName, children, ...props}: ActiveLinkProps) {
  const {asPath, isReady} = useRouter();
  const child = Children.only<JSX.Element>(children as JSX.Element);
- const childClassName = child.props.className || '';
- const [className, setClassName] = useState(child);
+ const childClassName = `${child.props.className || ''} ${defaultClassName}`;
+ const [className, setClassName] = useState(childClassName);
 
  useEffect(() => {
   if (isReady) {
--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import {cls} from '@libs/utils';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
 import {useCallback} from 'react';
+import ActiveLink from './ActiveLink';
 
 interface LayOutProps {
  title?: string;
@@ -31,7 +32,23 @@ export default function Layout({title, canGoBack, hasTabBar, children, seoTitle}
     {title && <span className={cls(canGoBack ? 'mx-auto' : '', '')}>{title}</span>}
    </div>
    <div className={cls('pt-12', hasTabBar ? 'pb-24' : '')}>{children}</div>
-   {hasTabBar && <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">menu</nav>}
+   {hasTabBar && (
+    <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
+     <ActiveLink activeClassName="text-orange-500" defaultClassName="hover:text-gray-500 transition-colors" href="/">
+      <a className={cls('flex flex-col items-center space-y-2')}>
+       <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path
+         strokeLinecap="round"
+         strokeLinejoin="round"
+         strokeWidth="2"
+         d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        ></path>
+       </svg>
+       <span>홈</span>
+      </a>
+     </ActiveLink>
+    </nav>
+   )}
   </div>
  );
 }
