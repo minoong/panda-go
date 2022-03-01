ddf471a1bb84a6e0d02e1b86fa5a827b14fcb0e6
lmw <alsdndjdrktl@gmail.com>
2022-03-01 19:40:47 +0900

    feat : common link component 구현

---
+++ components/ActiveLink.tsx
@@ -0,0 +1,35 @@
+import Link, {LinkProps} from 'next/link';
+import {useRouter} from 'next/router';
+import {Children, cloneElement, useEffect, useState} from 'react';
+
+interface ActiveLinkProps extends React.PropsWithChildren<LinkProps> {
+ activeClassName: string;
+ children: React.ReactNode;
+}
+
+export default function ActiveLink({activeClassName, children, ...props}: ActiveLinkProps) {
+ const {asPath, isReady} = useRouter();
+ const child = Children.only<JSX.Element>(children as JSX.Element);
+ const childClassName = child.props.className || '';
+ const [className, setClassName] = useState(child);
+
+ useEffect(() => {
+  if (isReady) {
+   const linkPathname = new URL((props.as as URL) || props.href, location.href).pathname;
+   const activePathname = new URL(asPath, location.href).pathname;
+   const newClassName = linkPathname === activePathname ? `${childClassName} ${activeClassName}`.trim() : childClassName;
+
+   if (newClassName !== className) {
+    setClassName(newClassName);
+   }
+  }
+ }, [asPath, isReady, props.as, props.href, childClassName, activeClassName, setClassName, className]);
+
+ return (
+  <Link {...props}>
+   {cloneElement(child, {
+    className: className || null,
+   })}
+  </Link>
+ );
+}
