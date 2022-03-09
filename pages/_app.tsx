f761ca1852fff6db9a4cfb64b2268fbbf0eae684
lmw <alsdndjdrktl@gmail.com>
2022-03-09 16:58:24 +0900

    feat : react 18 rc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {SWRConfig} from 'swr';
+import Script from 'next/script';
 
 function MyApp({Component, pageProps}: AppProps) {
+ console.log('APP IS RUNNING');
  return (
-  <SWRConfig value={{fetcher: (url: string) => fetch(url).then((res) => res.json())}}>
+  <SWRConfig
+   value={{
+    fetcher: (url: string) => fetch(url).then((response) => response.json()),
+   }}
+  >
    <div className="w-full max-w-xl mx-auto">
     <Component {...pageProps} />
    </div>
