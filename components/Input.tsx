5edb9fab4d6adb3f90292257c267fee2866bba3c
lmw <alsdndjdrktl@gmail.com>
2022-03-10 20:33:24 +0900

    feat : profile edit layout

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,5 +1,5 @@
 import {LinkProps} from 'next/link';
-import {InputHTMLAttributes} from 'react';
+import {InputHTMLAttributes, useEffect, useRef} from 'react';
 import type {UseFormRegisterReturn} from 'react-hook-form';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
  label: string;
@@ -8,6 +8,12 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default function Input({label, name, kind = 'text', register, required, type, ...rest}: InputProps) {
+ //  const ref = useRef<HTMLInputElement>(null);
+
+ //  useEffect(() => {
+ //   ref.current?.focus();
+ //  }, []);
+
  return (
   <div>
    <label className="mb-1 block text-sm font-medium text-gray-700" htmlFor={name}>
@@ -22,6 +28,7 @@ export default function Input({label, name, kind = 'text', register, required, t
       type={type}
       className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
       {...rest}
+      //   ref={ref}
      />
     </div>
    ) : null}
@@ -37,6 +44,7 @@ export default function Input({label, name, kind = 'text', register, required, t
       type={type}
       className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
       {...rest}
+      //   ref={ref}
      />
      <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
       <span className="text-gray-500">KRW</span>
@@ -55,6 +63,7 @@ export default function Input({label, name, kind = 'text', register, required, t
       type={type}
       className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
       {...rest}
+      //   ref={ref}
      />
     </div>
    ) : null}
