df3513bd410d1180a9c0d9d2f5777e0dfd252082
lmw <alsdndjdrktl@gmail.com>
2022-03-15 17:39:26 +0900

    feat : tailwind setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../styles/styles.css';
 import type {AppProps} from 'next/app';
 import {SWRConfig} from 'swr';
 import Script from 'next/script';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
  content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
  theme: {
-  extend: {},
+  extend: {
+   keyframes: {
+    'toast-in-bottom': {
+     from: {
+      transform: 'translateY(100%)',
+     },
+     to: {
+      transform: 'translateY(0)',
+     },
+    },
+   },
+   animation: {
+    't-in-bottom': 'toast-in-bottom .7s',
+   },
+  },
  },
  darkMode: 'media', // class
  plugins: [require('@tailwindcss/forms')],
