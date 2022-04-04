66ebbcf7fe876c48e509893498e64e50e4c3073b
lmw <alsdndjdrktl@gmail.com>
2022-04-04 20:39:13 +0900

    feat : 공통 컴포넌트 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import type {NextPage} from 'next';
 import Head from 'next/head';
 import client from '@libs/server/client';
 import {Product} from '@prisma/client';
-import {SWRConfig} from 'swr';
+import useSWR, {SWRConfig} from 'swr';
+import useUser from '@libs/client/useUser';
+import Item from '@components/item';
+import FloatingButton from '@components/floating-button';
 
 export interface ProductWithCount extends Product {
  _count: {
@@ -17,13 +20,24 @@ interface ProductsResponse {
 }
 
 const Home: NextPage = () => {
+ const {user, isLoading} = useUser();
+ const {data} = useSWR<ProductsResponse>('/api/products');
  return (
-  <Layout title="Home" hasTabBar canGoBack>
+  <Layout title="홈" hasTabBar>
    <Head>
     <title>Home</title>
    </Head>
    <div className="flex flex-col space-y-5 divide-y">
-    <div>test!</div>
+    {data
+     ? data?.products?.map((product) => (
+        <Item id={product.id} key={product.id} title={product.name} price={product.price} hearts={product._count?.favs || 0} image={product.image} />
+       ))
+     : 'Loading...'}
+    <FloatingButton href="/products/upload">
+     <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+     </svg>
+    </FloatingButton>
    </div>
   </Layout>
  );
