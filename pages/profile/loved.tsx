1de846104a7137a69c21ff46f62b8dba67f2da96
lmw <alsdndjdrktl@gmail.com>
2022-04-12 20:19:29 +0900

    feat : 관심상품 API, component 구현

--- pages/profile/loved.tsx
+++ pages/profile/loved.tsx
@@ -1,9 +1,12 @@
 import Layout from '@components/layout';
+import ProductList from '@components/product-list';
 import {NextPage} from 'next';
 
 const Loved: NextPage = () => (
  <Layout title="관심목록" canGoBack>
-  <div className="flex flex-col space-y-5 pb-10  divide-y">{/* <ProductList kind="sales" /> */}</div>
+  <div className="flex flex-col space-y-5 pb-10  divide-y">
+   <ProductList kind="favs" />
+  </div>
  </Layout>
 );
 
