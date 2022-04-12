0ad936dc3f07943c7ea0c9f671107f589673c120
lmw <alsdndjdrktl@gmail.com>
2022-04-12 20:24:30 +0900

    feat : 판매상품 API, component 구현

--- pages/profile/bought.tsx
+++ pages/profile/bought.tsx
@@ -1,9 +1,12 @@
 import Layout from '@components/layout';
+import ProductList from '@components/product-list';
 import {NextPage} from 'next';
 
 const Bought: NextPage = () => (
  <Layout title="구매내역" canGoBack>
-  <div className="flex flex-col space-y-5 pb-10  divide-y">{/* <ProductList kind="sales" /> */}</div>
+  <div className="flex flex-col space-y-5 pb-10  divide-y">
+   <ProductList kind="sales" />
+  </div>
  </Layout>
 );
 
