729dfa54883512aa8d88403eed9a6ce0333d2eda
lmw <alsdndjdrktl@gmail.com>
2022-04-13 21:44:01 +0900

    fix : PATH 수정

--- containers/nav/MenuList.tsx
+++ containers/nav/MenuList.tsx
@@ -7,11 +7,11 @@ import {FaRegCommentDots} from 'react-icons/fa';
 
 const MenuList = () => {
  return (
-  <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
+  <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 py-4 flex justify-between text-xs">
    <Menu path="/" Icon={BiHome} name={{en: 'home', ko: '홈'}} />
-   <Menu path="market" Icon={BsCurrencyBitcoin} name={{en: 'market', ko: '마켓'}} />
-   <Menu path="community" Icon={FaRegCommentDots} name={{en: 'community', ko: '커뮤니티'}} />
-   <Menu path="profile" Icon={CgProfile} name={{en: 'profile', ko: '프로파일'}} />
+   <Menu path="/market" Icon={BsCurrencyBitcoin} name={{en: 'market', ko: '마켓'}} />
+   <Menu path="/community" Icon={FaRegCommentDots} name={{en: 'community', ko: '커뮤니티'}} />
+   <Menu path="/profile" Icon={CgProfile} name={{en: 'profile', ko: '프로파일'}} />
   </nav>
  );
 };
