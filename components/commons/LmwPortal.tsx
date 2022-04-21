244784ca999b1a883a38b550471659fc38f772ba
lmw <alsdndjdrktl@gmail.com>
2022-04-21 21:15:54 +0900

    feat : create portal

---
+++ components/commons/LmwPortal.tsx
@@ -0,0 +1,37 @@
+import React, {useState, useLayoutEffect} from 'react';
+import {createPortal} from 'react-dom';
+
+interface LmwPortalProps {
+ id?: string;
+ children: React.ReactNode;
+}
+
+function createWrapperAndAppendToBody(id: string) {
+ const wrapperElement = document.createElement('div');
+ wrapperElement.setAttribute('id', id);
+ document.body.appendChild(wrapperElement);
+
+ return wrapperElement;
+}
+
+const LmwPortal: React.FC<LmwPortalProps> = ({id = 'lmw-portal-wrapper', children}) => {
+ const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(null);
+
+ useLayoutEffect(() => {
+  const element = document.getElementById(id) || createWrapperAndAppendToBody(id);
+  const systemCreated = !element ? true : false;
+  setWrapperElement(element);
+
+  return () => {
+   if (systemCreated && element.parentNode) {
+    element.parentNode.removeChild(element);
+   }
+  };
+ }, [id]);
+
+ if (wrapperElement === null) return null;
+
+ return createPortal(children, wrapperElement);
+};
+
+export default LmwPortal;
