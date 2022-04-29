2053185d00afb483426eadda2a3f0b0f1fca912a
lmw <alsdndjdrktl@gmail.com>
2022-04-29 20:06:23 +0900

    feat : thirdweb install and thirdweb Provider setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,26 +5,31 @@ import Script from 'next/script';
 import Providers from '@components/toast/Providers';
 import {RecoilRoot} from 'recoil';
 import Modal from '@components/commons/modal/Modal';
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
+
+const activeChainId = ChainId.Rinkeby;
 
 function MyApp({Component, pageProps}: AppProps) {
  console.log('APP IS RUNNING');
  return (
   <>
    <Script src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}&callback=initMap`} />
-   <RecoilRoot>
-    <SWRConfig
-     value={{
-      fetcher: (url: string) => fetch(url).then((response) => response.json()),
-     }}
-    >
-     <Providers>
-      <div className="w-full max-w-xl mx-auto h-screen relative" id="page">
-       <Component {...pageProps} />
-       <Modal />
-      </div>
-     </Providers>
-    </SWRConfig>
-   </RecoilRoot>
+   <ThirdwebProvider desiredChainId={activeChainId}>
+    <RecoilRoot>
+     <SWRConfig
+      value={{
+       fetcher: (url: string) => fetch(url).then((response) => response.json()),
+      }}
+     >
+      <Providers>
+       <div className="w-full max-w-xl mx-auto h-screen relative" id="page">
+        <Component {...pageProps} />
+        <Modal />
+       </div>
+      </Providers>
+     </SWRConfig>
+    </RecoilRoot>
+   </ThirdwebProvider>
   </>
  );
 }
