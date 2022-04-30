a7267ec328eee54c48e9f754696795565ac9dc31
lmw <alsdndjdrktl@gmail.com>
2022-04-30 15:45:34 +0900

    refactor : modal 높이 옵션 추가

--- components/commons/modal/Modal.tsx
+++ components/commons/modal/Modal.tsx
@@ -4,11 +4,20 @@ import {useRecoilState} from 'recoil';
 import {modalState} from 'recoil/modal';
 import LmwPortal from '../LmwPortal';
 
+type ModalHeightProps = 'full' | 'half' | 'auto';
+
 export interface ModalProps {
  isOpen: boolean;
  handleClose?: () => void;
  children?: React.ReactNode;
+ height?: ModalHeightProps;
 }
+
+const heightConfig: Record<ModalHeightProps, string> = {
+ full: 'h-screen',
+ auto: '',
+ half: 'h-1/2',
+};
 
 const Modal: React.FC = () => {
  const {modalProps} = useRecoilState(modalState)[0] || {};
@@ -22,7 +31,7 @@ const Modal: React.FC = () => {
   document.body.addEventListener('keydown', closeOnEscapeKey);
 
   return () => document.body.removeEventListener('keydown', closeOnEscapeKey);
- }, []);
+ }, [onDisableModal]);
 
  if (!modalProps?.isOpen) return null;
 
@@ -30,7 +39,11 @@ const Modal: React.FC = () => {
   <LmwPortal>
    <div className="lmw-modal">
     <div className="bg-black h-full fixed top-0 left-0 w-full bg-opacity-75 z-40" onClick={onDisableModal} />
-    <div className="bg-white min-h-[150px] bottom-0 absolute right-0 left-0 toast-bottom rounded-t-lg z-50 py-6 px-6">{modalProps.children}</div>
+    <div
+     className={`bg-white min-h-[150px] bottom-0 absolute right-0 left-0 toast-bottom rounded-t-lg z-50 py-6 px-6 ${heightConfig[modalProps.height || 'auto']}`}
+    >
+     {modalProps.children}
+    </div>
    </div>
   </LmwPortal>
  );
